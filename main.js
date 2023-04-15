836ba6b37dd8e940ec5b9563033014d66a04d1da
dmitr_zubakin <daz371885bb@gmail.com>
2023-04-15 15:27:30 +0200

    update cv from gulp to vite

---
+++ main.js
@@ -0,0 +1,10 @@
+import './styles/style.scss'
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <h1>Hello World!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//   </div>
+// `
+//
