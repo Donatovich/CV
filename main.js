f1542d03655fa0388d62ddbc7a96cd7c83020f91
Donatovich <astrouski1994@gmail.com>
2023-04-20 19:44:43 +0300

    last

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
 import './styles/style.scss'
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <h1>Hello World!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//   </div>
-// `
-//
