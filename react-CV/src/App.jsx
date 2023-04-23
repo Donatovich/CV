9ead45543fe5ab08e9051e62345c973a12256778
dmitr_zubakin <daz371885bb@gmail.com>
2023-04-23 12:01:56 +0200

    fix: update styles

--- react-CV/src/App.jsx
+++ react-CV/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import './App.scss'
+import './styles/App.module.scss'
 import Aside from "./components/aside/Aside.jsx";
 import Header from "./components/header/Header";
 import AboutSection from "./components/aboutSection/AboutSection";
--- react-CV/src/components/aside/Aside.jsx
+++ react-CV/src/components/aside/Aside.jsx
@@ -5,6 +5,7 @@ import tgIcon from "../../assets/icons/tg.png";
 import fbIcon from "../../assets/icons/fb.png";
 import vkIcon from "../../assets/icons/vk1.png";
 import ghIcon from "../../assets/icons/gh1.png";
+
 const Aside = () => {
     return (
             <aside className={styles.aside}>
@@ -14,28 +15,42 @@ const Aside = () => {
                     </div>
                     <h2 className={styles.asideSection__fullname}>Egor Ostrovskiy</h2>
                     <p className={styles.asideSection__text}>Frontend developer</p>
-                    <div className="aside-section__list">
-                        <p className="aside-section__link">
-
-                            <a href="https://t.me/astrouski"><img src={tgIcon} width="35" height="35"
-                                                                  alt=""/></a>
+                    <div className={styles.asideSection__list}>
+                        <p className={styles.asideSection__link}>
+                            <a href="https://t.me/astrouski">
+                                <img
+                                    src={tgIcon}
+                                    width="35"
+                                    height="35"
+                                    alt=""/></a>
                         </p>
-                        <p className="aside-section__link">
-                            <a href="https://www.facebook.com/profile.php?id=100024575885419"><img
-                                src={fbIcon} width="40" height="40" alt=""/></a>
+                        <p className={styles.asideSection__link}>
+                            <a href="https://www.facebook.com/profile.php?id=100024575885419">
+                                <img src={fbIcon} width="40" height="40" alt=""/>
+                            </a>
                         </p>
-                        <p className="aside-section__link">
-                            <a href="https://vk.com/ega100500"><img src={vkIcon} width="40"
-                                                                    height="40" alt=""/></a>
+                        <p className={styles.asideSection__link}>
+                            <a href="https://vk.com/ega100500">
+                                <img
+                                    src={vkIcon}
+                                    width="40"
+                                    height="40"
+                                    alt=""/>
+                            </a>
                         </p>
-                        <p className="aside-section__link">
-                            <a href="https://github.com/Donatovich"><img src={ghIcon} width="35"
-                                                                         height="35" alt=""/></a>
+                        <p className={styles.asideSection__link}>
+                            <a href="https://github.com/Donatovich">
+                                <img
+                                    src={ghIcon}
+                                    width="35"
+                                    height="35"
+                                    alt=""/>
+                            </a>
                         </p>
                     </div>
                 </section>
-                <div className="aside-button">
-                    <button className="aside-button__download">Download this CV</button>
+                <div className={styles.asideButton}>
+                    <button className={styles.asideButton__download}>Download this CV</button>
                 </div>
             </aside>
     );
